3197c4eb464b28de2f1d2e8fdaa397ef8cb42610
simpja <simenpjaat@gmail.com>
2019-09-01 00:07:24 +0200

    chatting

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 var app = require("express")();
 var http = require("http").createServer(app);
 var io = require("socket.io")(http);
+const CHAT_EVENT = "chat message";
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
 io.on("connection", function(socket) {
-  console.log("a user connected");
+  io.emit(CHAT_EVENT, "server: someone connected");
+
+  socket.on(CHAT_EVENT, event => {
+    console.log(event);
+    io.emit(CHAT_EVENT, event);
+  });
+
+  socket.on("disconnect", reason => {
+    console.log("WTFTWFWTFWTF ", reason);
+    io.emit(CHAT_EVENT, "server: someone disconnected");
+  });
 });
 
 http.listen(3000, function() {
